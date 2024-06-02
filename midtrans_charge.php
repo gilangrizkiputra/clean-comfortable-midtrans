93ca20bd2fcb39a82873e91f03429097626be5d7
gilangrizkiputra <gilangrizkiptra@gmail.com>
2024-06-02 19:26:11 +0700

    update

--- midtrans_charge.php
+++ midtrans_charge.php
@@ -1,13 +1,4 @@
 <?php
-// Setel server key Anda (Catatan: Server key untuk mode sandbox dan produksi berbeda)
-$server_key = 'SB-Mid-server-Xzba3_5u-lTBv-e71pfEXQSw';
-// Setel true untuk produksi, setel false untuk sandbox
-$is_production = false;
-
-$api_url = $is_production ? 
-  'https://app.midtrans.com/snap/v1/transactions' : 
-  'https://app.sandbox.midtrans.com/snap/v1/transactions';
-
 // Cek jika metode bukan HTTP POST, tampilkan 404
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
   http_response_code(404);
@@ -16,51 +7,13 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
 
 // Mendapatkan body dari HTTP POST request
 $request_body = file_get_contents('php://input');
+
 // Setel tipe konten response sebagai JSON
 header('Content-Type: application/json');
-// Panggil API charge menggunakan body permintaan yang diberikan oleh mobile SDK
-$charge_result = chargeAPI($api_url, $server_key, $request_body);
 
-// Log hasil response untuk debugging
-error_log("Hasil API Charge: " . print_r($charge_result, true));
+// Cek dan log request body
+error_log("Request Body: " . $request_body);
 
-// Setel kode status HTTP response
-http_response_code($charge_result['http_code']);
-// Kemudian cetak body response
-echo $charge_result['body'];
-
-/**
- * Panggil API charge menggunakan Curl
- * @param string  $api_url
- * @param string  $server_key
- * @param string  $request_body
- */
-function chargeAPI($api_url, $server_key, $request_body){
-  $ch = curl_init();
-  $curl_options = array(
-    CURLOPT_URL => $api_url,
-    CURLOPT_RETURNTRANSFER => 1,
-    CURLOPT_POST => 1,
-    CURLOPT_HEADER => 0,
-    // Tambahkan header ke permintaan, termasuk Authorization yang dihasilkan dari server key
-    CURLOPT_HTTPHEADER => array(
-      'Content-Type: application/json',
-      'Accept: application/json',
-      'Authorization: Basic ' . base64_encode($server_key . ':')
-    ),
-    CURLOPT_POSTFIELDS => $request_body
-  );
-  curl_setopt_array($ch, $curl_options);
-  $body = curl_exec($ch);
-  $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-  if (curl_errno($ch)) {
-    $body = json_encode(["error" => curl_error($ch)]);
-    $http_code = 500;
-  }
-  curl_close($ch);
-  return array(
-    'body' => $body,
-    'http_code' => $http_code,
-  );
-}
+// Response dengan request body yang diterima
+echo json_encode(["message" => "Permintaan POST diterima", "data" => json_decode($request_body)]);
 ?>
